2221e207924566501f36f4adbee2d8b2be28e6a9
Piyush-Guptos <intensioecho@gmail.com>
2024-08-05 11:42:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+// scripts.js
+
+// Ripple effect function
+document.querySelectorAll('.button, .button-secondary, .button-tertiary').forEach(button => {
+    button.addEventListener('click', function (e) {
+        const rect = this.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+        const ripple = document.createElement('span');
+        ripple.style.left = `${x}px`;
+        ripple.style.top = `${y}px`;
+        this.appendChild(ripple);
+        setTimeout(() => ripple.remove(), 600);
+    });
+});
+
+// script.js for the hamburgar menu
+
+    document.addEventListener('DOMContentLoaded', function () {
+        const menuToggle = document.querySelector('.menu-toggle');
+        const navLinks = document.querySelector('.nav-links');
+
+        menuToggle.addEventListener('click', function () {
+            navLinks.classList.toggle('active');
+        });
+    });
+
+
+
